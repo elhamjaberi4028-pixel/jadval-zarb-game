8390c705e9765084bf3df47d45f8c3f3e6af856c
elhamjaberi4028-pixel <elham.jaberi4028@gmail.com>
2026-08-03 17:11:23 +0330

    Update script.js

--- script.js
+++ script.js
@@ -503,10 +503,10 @@ ${zoo}
 
 <div class="question" style="direction:ltr; unicode-bidi:bidi-override;">
 
-? = ${q.a.toLocaleString("fa-IR")} × ${q.b.toLocaleString("fa-IR")}
+<span>${q.a.toLocaleString("fa-IR")} × ${q.b.toLocaleString("fa-IR")}</span>
+<span> = ?</span>
 
 </div>
-
 <div class="answers">
 
 ${options.map(x=>`

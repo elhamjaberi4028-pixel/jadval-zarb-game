2ca2b39172edeb568e579777a8f9dc78fe730699
elhamjaberi4028-pixel <elham.jaberi4028@gmail.com>
2026-08-03 16:27:16 +0330

    Update script.js

--- جدول ضرب/script.js
+++ جدول ضرب/script.js
@@ -502,12 +502,12 @@ ${zoo}
 
 
 
-<div class="question" style="direction:ltr; unicode-bidi:plaintext;">
+<div class="question">
 
-? = ${q.a.toLocaleString("fa-IR")} × ${q.b.toLocaleString("fa-IR")}
+<span style="direction:ltr; unicode-bidi:isolate;">? = </span>
+<span>${q.a.toLocaleString("fa-IR")} × ${q.b.toLocaleString("fa-IR")}</span>
 
 </div>
-
 <div class="answers">
 
 ${options.map(x=>`
